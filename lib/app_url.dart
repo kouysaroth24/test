98b6258c6c25153b30bdc6546c9b5ea3180ac25e
kouysaroth24 <kouysaroth@gmail.com>
2024-12-07 12:27:13 +0700

    commit message

---
+++ lib/app_url.dart
@@ -0,0 +1,6 @@
+class AppUrl {
+  //static const url ="http://127.0.0.1:8080/BmcBbu404_API/";
+    static const url ="http://10.0.2.2:8080/BmcBbu404_API/";
+  //  static const url ="http://localhost:8080/BmcBbu404_API/";
+
+}
---
+++ lib/screen/app_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const blue = Color.fromARGB(255, 102, 61, 214);
+}
